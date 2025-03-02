5d7215c6e39a3975e485c69a766632e1d706d86f
George Wild <git@georgewild.dev>
2025-03-02 17:39:02 +0000

    Fixed Spelling

--- static/script.js
+++ static/script.js
@@ -1,33 +1,33 @@
-
-const LINES = [["where", "rats, rats, as big as bloomin' cats"],
-["was", "gravy, gravy, enough to sink the Navy"],
-["where", "beans, beans, big as submarines"],
-["was", "cheese, cheese, that smelled like sailors knees"],
-["where", "peas, peas, with kilts and hairy knees"],
-["where", "snakes, snakes, as big as garden rakes"],
-["where", "goats, goats, eating all the oats"],
-["where", "chips, chips, as big as battleships"]
+const LINES = [
+  ["where", "rats, rats, as big as bloomin' cats"],
+  ["was", "gravy, gravy, enough to sink the Navy"],
+  ["were", "beans, beans, big as submarines"],
+  ["was", "cheese, cheese, that smelled like sailors knees"],
+  ["were", "peas, peas, with kilts and hairy knees"],
+  ["were", "snakes, snakes, as big as garden rakes"],
+  ["were", "goats, goats, eating all the oats"],
+  ["were", "chips, chips, as big as battleships"],
 ];
 
 let PREVIOUS;
 function getRandomInt(amount_posible) {
-    return Math.floor(Math.random() * amount_posible);
+  return Math.floor(Math.random() * amount_posible);
 }
 function getLine(line_number) {
-    return LINES[line_number];
+  return LINES[line_number];
 }
 function getRandomLine() {
-    let line_number = getRandomInt(LINES.length);
-    while (line_number == PREVIOUS) {
-        line_number = getRandomInt(LINES.length);
-    }
-    PREVIOUS = line_number;
-    let line = getLine(line_number);
-    return line;
+  let line_number = getRandomInt(LINES.length);
+  while (line_number == PREVIOUS) {
+    line_number = getRandomInt(LINES.length);
+  }
+  PREVIOUS = line_number;
+  let line = getLine(line_number);
+  return line;
 }
 
 function generate() {
-    line = getRandomLine()
-    document.getElementById("word").innerHTML = line[0];
-    document.getElementById("line").innerHTML = line[1];
+  line = getRandomLine();
+  document.getElementById("word").innerHTML = line[0];
+  document.getElementById("line").innerHTML = line[1];
 }
